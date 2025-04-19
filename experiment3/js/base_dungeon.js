6fd99d0f5f27fa0349ac584e9b49468b7488cc0b
ljcui1 <yangyangjiaocui@gmail.com>
2025-04-18 20:17:42 -0700

    Finished experiment 3 (no description)

--- experiment3/js/base_dungeon.js
+++ experiment3/js/base_dungeon.js
@@ -7,24 +7,10 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
+(function() {
 // Globals
-let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
 
 function resizeScreen() {
   centerHorz = width / 2; // Adjusted for drawing logic
@@ -46,7 +32,7 @@ let numRows, numCols;
 //vignette graphic variable
 let vignetteGraphics;
 
-function preload() {
+window.preload = function() {
   tilesetImage = loadImage(
     "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2Ftileset.png?v=1611654020438"
   );
@@ -91,7 +77,7 @@ function stringToGrid(str) {
   return grid;
 }
 
-function setup() {
+window.setup = function() {
   numCols = select("#asciiBoxD").attribute("rows") | 0;
   numRows = select("#asciiBoxD").attribute("cols") | 0;
 
@@ -113,7 +99,7 @@ function setup() {
 }
 
 
-function draw() {
+window.draw = function() {
   randomSeed(seed);
   drawGrid(currentGrid);
 
@@ -126,31 +112,250 @@ function placeTile(i, j, ti, tj) {
 }
 
 function drawVignette() {
-  //clearing and setting up vignette
-  vignetteGraphics.clear();
-  vignetteGraphics.noStroke();
+  //put vignette pixels into array
+  vignetteGraphics.loadPixels();
+  let d = vignetteGraphics.pixelDensity();
+  let w = vignetteGraphics.width;
+  let h = vignetteGraphics.height;
 
-  //time variable to move vignette by
+  //perlin noise movement based on time
   let time = millis();
+  let offsetX = noise(time * 0.0005) * 100 - 50;
+  let offsetY = noise(time * 0.0005 + 100) * 100 - 50;
 
-  //move vignette position with perlin noise based on time
-  let offsetX = noise(time) * 100 - 50;
-  let offsetY = noise(time + 100) * 100 - 50;
-
-  //nested loop over every pixel on canvas and calculate distance from center to change the alpha value of the vignette
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      let dx = (x - width / 2 + offsetX) / (width / 2);
-      let dy = (y - height / 2 + offsetY) / (height / 2);
-
+  //changing alpha value of pixels depending on distance to center
+  for (let x = 0; x < w; x++) {
+    for (let y = 0; y < h; y++) {
+      let dx = (x - w / 2 + offsetX) / (w / 2);
+      let dy = (y - h / 2 + offsetY) / (h / 2);
       let dSQ = dx * dx + dy * dy;
 
       let alpha = map(dSQ, 0.4, 1.0, 0, 150);
       alpha = constrain(alpha, 0, 150);
 
-      vignetteGraphics.fill(0, alpha);
-      vignetteGraphics.rect(x, y, 1, 1);
+      for (let i = 0; i < d; i++) {
+        for (let j = 0; j < d; j++) {
+          let index = 4 * ((y * d + j) * w * d + (x * d + i));
+          vignetteGraphics.pixels[index + 0] = 0;
+          vignetteGraphics.pixels[index + 1] = 0;
+          vignetteGraphics.pixels[index + 2] = 0;
+          vignetteGraphics.pixels[index + 3] = alpha;
+        }
+      }
     }
   }
+
+  vignetteGraphics.updatePixels();
   image(vignetteGraphics, 0, 0);
 }
+
+/* exported generateGrid, drawGrid */
+/* global placeTile */
+
+function generateGrid(numCols, numRows) {
+  let grid = [];
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 0; j < numCols; j++) {
+      row.push("S");
+    }
+    grid.push(row);
+  }
+  
+  //room restrictions
+  let rooms = [];
+  let roomNum = 7;
+  let minRoomSize = 2;
+  let maxRoomSize = 8;
+  
+  //random rooms
+  for (let i = 0; i < roomNum; i++) {
+    let w = Math.floor(random(minRoomSize, maxRoomSize));
+    let h = Math.floor(random(minRoomSize, maxRoomSize));
+    let x = Math.floor(random(0, numCols - w));
+    let y = Math.floor(random(0, numRows - h));
+    
+    //make and store room in array
+    for (let s = y; s < y + h; s++) {
+      for (let t = x; t < x + w; t++) {
+        grid[s][t] = "D";
+      }
+    }
+    
+    rooms.push({x: x + Math.floor(w / 2), y: y + Math.floor(h / 2) });
+  
+      // add a chest randomly in the rooms
+      if (random() < 0.5) {
+          let chestX = Math.floor(random(x + 1, x + w - 1));
+          let chestY = Math.floor(random(y + 1, y + h - 1));
+          grid[chestY][chestX] = "C";
+      }
+  }
+  
+  //connect rooms with paths
+  for (let i = 1; i < rooms.length; i++) {
+    let a = rooms[i - 1];
+    let b = rooms[i];
+    
+    //horizontal path
+    for (let x = Math.min(a.x, b.x); x <= Math.max(a.x, b.x); x++) {
+      if (grid[a.y][x] !== "D") grid[a.y][x] = "P"; 
+    }
+    //vertical path
+    for (let y = Math.min(a.y, b.y); y <= Math.max(a.y, b.y); y++) {
+      if (grid[y][b.x] !== "D") grid[y][b.x] = "P"; 
+    }
+  }
+  //random generate towers
+  for (let h = 0; h < random(3, 20); h++) {
+      let placed = false;
+      while (!placed) {
+          let x = Math.floor(random(grid.length));
+          let y = Math.floor(random(grid[0].length));
+
+          if (grid[x][y] === "S") {
+              grid[x][y] = "T";
+              placed = true;
+          }
+      }
+  }
+
+  return grid;
+}
+
+function drawGrid(grid) {
+  background(128);
+
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid[i].length; j++) {
+      //grass
+      if (grid[i][j] == 'S') {
+        placeTile(i, j, (floor(random(4))), 12);
+      }
+      //room
+      if (gridCheck(grid, i, j, "D")) {
+        placeTile(i, j, (floor(random(11, 14))), (floor(random(22, 25))));
+        drawContext(grid, i, j, "S", 9, 12);
+        drawContext(grid, i, j, "T", 9, 12);
+      }
+
+      //path tile
+      if (gridCheck(grid, i, j, "P")) {
+        placeTile(i, j, 8, 24);
+      }
+
+      //chest tile
+      if (gridCheck(grid, i, j, "C")) {
+        placeTile(i, j, (floor(random(11, 14))), (floor(random(22, 25))));
+        placeTile(i, j, floor(random(3, 5)), floor(random(28, 30)));
+      }
+
+      //tower tile
+      if (gridCheck(grid, i, j, "T")) {
+        let yTile = random([1, 3]);
+        let xTile = floor(random(28, 30));
+        placeTile(i, j, (floor(random(4))), 12);
+        placeTile(i, j, xTile, yTile);
+        if ((i-1) != null) {
+            placeTile(i-1, j, xTile, yTile-1);
+        }
+      }
+      
+      /*if(grid[i][j] == '.'){
+        //room floor
+        placeTile(i, j, (floor(random(11, 14))), (floor(random(22, 25))));
+        //room wall
+        if(grid[i-1][j] == '_'){
+          //top
+          placeTile(i, j, 5, 0);
+          //corners (left then right)
+          if(grid[i][j-1] == '_'){
+            placeTile(i, j, 4, 0);
+          }
+          if(grid[i][j+1] == '_'){
+            placeTile(i, j, 6, 0);
+          }
+        }
+        
+        if(grid[i+1][j] == '_'){
+          //bottom
+          placeTile(i, j, 5, 2);
+          //corners (left then right)
+          if(grid[i][j-1] == '_'){
+            placeTile(i, j, 4, 2);
+          }
+          if(grid[i][j+1] == '_'){
+            placeTile(i, j, 6, 2);
+          }
+        }
+        
+        if(grid[i][j-1] == '_'){
+          //left
+          placeTile(i, j, 4, 1);
+        }
+        if(grid[i][j+1] == '_'){
+          //right
+          placeTile(i, j, 6, 1);
+        }
+      }*/
+    }
+  }
+}
+
+//If location i,j is inside the grid (not out of bounds), does grid[i][j]==target? Otherise, return false.
+function gridCheck(grid, i, j, target) {
+  if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+    return false;
+  }
+  return grid[i][j] === target;
+}
+
+//Form a 4-bit code using gridCheck on the north/south/east/west neighbors of i,j for the target code. You might use an example like (northBit<<0)+(southBit<<1)+(eastBit<<2)+(westBit<<3).
+function gridCode(grid, i, j, target) {
+  let northBit = gridCheck(grid, i-1, j, target) ? 1 : 0;
+  let southBit = gridCheck(grid, i+1, j, target) ? 1 : 0;
+  let eastBit = gridCheck(grid, i, j+1, target) ? 1 : 0;
+  let westBit = gridCheck(grid, i, j-1, target) ? 1 : 0;
+
+  return (northBit << 0) + (southBit << 1) + (eastBit << 2) + (westBit << 3);
+}
+
+//Get the code for this location and target. Use the code as an array index to get a pair of tile offset numbers.
+function drawContext(grid, i, j, target, dti, dtj) {
+  const code = gridCode(grid, i, j, target);
+  const [tiOffset, tjOffset] = lookup[code];
+
+  //calculate tile position offset for correct border placement
+  let northBit = gridCheck(grid, i-1, j, target) ? 1 : 0;
+  let southBit = gridCheck(grid, i+1, j, target) ? 1 : 0;
+  let eastBit = gridCheck(grid, i, j+1, target) ? 1 : 0;
+  let westBit = gridCheck(grid, i, j-1, target) ? 1 : 0;
+  
+  //border offset
+  let xOffset = (-northBit) + (southBit);
+  let yOffset = (-eastBit) + (westBit);
+
+  placeTile(i, j, dti + tiOffset, dtj + tjOffset);
+}
+
+//A global variable referring to an array of 16 elements. Fill this with hand-typed tile offset pairs, e.g. [2,1], so that drawContext does not need to handle any special cases.
+const lookup = [
+  [1, 1],
+  [1, 0],
+  [1, 2],
+  [1, 1],
+  [2, 1],
+  [2, 0],
+  [2, 2],
+  [2, 1],
+  [0, 1],
+  [0, 0],
+  [0, 2],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, 2],
+  [1, 1]
+];
+
+})();
--- experiment3/js/base_overworld.js
+++ experiment3/js/base_overworld.js
@@ -7,24 +7,10 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
+new p5(function(p) {
 // Globals
-let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
 
 function resizeScreen() {
   centerHorz = width / 2; // Adjusted for drawing logic
@@ -43,7 +29,7 @@ let tilesetImage;
 let currentGrid = [];
 let numRows, numCols;
 
-function preload() {
+p.preload = function() {
   tilesetImage = loadImage(
     "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2Ftileset.png?v=1611654020438"
   );
@@ -51,19 +37,19 @@ function preload() {
 
 function reseed() {
   seed = (seed | 0) + 1109;
-  randomSeed(seed);
-  noiseSeed(seed);
-  select("#seedReportO").html("seed " + seed);
+  p.randomSeed(seed);
+  p.noiseSeed(seed);
+  p.select("#seedReportO").html("seed " + seed);
   regenerateGrid();
 }
 
 function regenerateGrid() {
-  select("#asciiBoxO").value(gridToString(generateGrid(numCols, numRows)));
+    p.select("#asciiBoxO").value(gridToString(generateGrid(numCols, numRows)));
   reparseGrid();
 }
 
 function reparseGrid() {
-  currentGrid = stringToGrid(select("#asciiBoxO").value());
+  currentGrid = stringToGrid(p.select("#asciiBoxO").value());
 }
 
 function gridToString(grid) {
@@ -88,17 +74,17 @@ function stringToGrid(str) {
   return grid;
 }
 
-function setup() {
-  numCols = select("#asciiBoxO").attribute("rows") | 0;
-  numRows = select("#asciiBoxO").attribute("cols") | 0;
+p.setup = function() {
+  numCols = p.select("#asciiBoxO").attribute("rows") | 0;
+  numRows = p.select("#asciiBoxO").attribute("cols") | 0;
 
-  canvasContainer = select("#canvasContainerO");
+  canvasContainer = p.select("#canvasContainerO");
 
-  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainerO");
-  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+  p.createCanvas(16 * numCols, 16 * numRows).parent("canvasContainerO");
+  p.select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
 
-  select("#reseedButtonO").mousePressed(reseed);
-  select("#asciiBoxO").input(reparseGrid);
+  p.select("#reseedButtonO").mousePressed(reseed);
+  p.select("#asciiBoxO").input(reparseGrid);
 
   reseed();
 
@@ -109,12 +95,231 @@ function setup() {
 }
 
 
-function draw() {
+p.draw = function() {
   randomSeed(seed);
   drawGrid(currentGrid);
 }
 
 function placeTile(i, j, ti, tj) {
-  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+    p.image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
 }
 
+/* exported generateGrid, drawGrid */
+/* global placeTile */
+
+function generateGrid(numCols, numRows) {
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+      let row = [];
+      for (let j = 0; j < numCols; j++) {
+        row.push("G");
+      }
+      grid.push(row);
+    }
+    
+    //water restrictions
+    let water = [];
+    let waterNum = 7;
+    let minWaterSize = 3;
+    let maxWaterSize = 10;
+    
+    //random water
+    for (let i = 0; i < waterNum; i++) {
+      let w = Math.floor(p.random(minWaterSize, maxWaterSize));
+      let h = Math.floor(p.random(minWaterSize, maxWaterSize));
+      let x = Math.floor(p.random(0, numCols - w));
+      let y = Math.floor(p.random(0, numRows - h));
+      
+      //make and store water in array
+      for (let s = y; s < y + h; s++) {
+        for (let t = x; t < x + w; t++) {
+          grid[s][t] = "W";
+        }
+      }
+      
+      water.push({x: x + Math.floor(w / 2), y: y + Math.floor(h / 2) });
+    
+        // add a ROCK randomly in the rooms
+        if (p.random() < 0.5) {
+            let chestX = Math.floor(p.random(x + 1, x + w - 1));
+            let chestY = Math.floor(p.random(y + 1, y + h - 1));
+            grid[chestY][chestX] = "R";
+        }
+    }
+    
+    //random generate houses
+    for (let h = 0; h < p.random(20, 30); h++) {
+        let placed = false;
+        while (!placed) {
+            let x = Math.floor(p.random(grid.length));
+            let y = Math.floor(p.random(grid[0].length));
+
+            if (grid[x][y] === "G") {
+                grid[x][y] = "H";
+                placed = true;
+            }
+        }
+    }
+
+    //random generate forests
+    for (let f = 0; f < p.random(5, 15); f++) {
+        //random grass tile
+        let x, y;
+        let tries = 0;
+        do {
+            x = p.floor(p.random(numCols));
+            y = p.floor(p.random(numRows));
+            tries++;
+        } while (grid[y][x] !== "G" && tries < 100);
+
+        // grow a small cluster around that point
+        for (let i = 0; i < p.random(5, 20); i++) {
+            let nx = x + p.floor(p.random(-2, 3));
+            let ny = y + p.floor(p.random(-2, 3));
+
+            if (
+            nx >= 0 && ny >= 0 &&
+            nx < numCols && ny < numRows &&
+            grid[ny][nx] === "G"
+            ) {
+            grid[ny][nx] = "T";
+            }
+        }
+    }
+
+    return grid;
+  }
+  
+  function drawGrid(grid) {
+    p.background(128);
+  
+    for(let i = 0; i < grid.length; i++) {
+      for(let j = 0; j < grid[i].length; j++) {
+        //grass
+        if (grid[i][j] == 'G') {
+          placeTile(i, j, (floor(random(4))), 0);
+        }
+        //WATER
+        if (gridCheck(grid, i, j, "W")) {
+          placeTile(i, j, (floor(random(4))), 13);
+          drawContext(grid, i, j, "G", 9, 0);
+          drawContext(grid, i, j, "H", 9, 0);
+          drawContext(grid, i, j, "T", 9, 0);
+        }
+
+        //rocks tile
+        if (gridCheck(grid, i, j, "R")) {
+            placeTile(i, j, (floor(random(4))), 13);
+            placeTile(i, j, 14, 9);
+        }
+
+        //house tile
+        if (gridCheck(grid, i, j, "H")) {
+            placeTile(i, j, 26, floor(random(4)));
+            
+        }
+
+        //tree tile
+        if (gridCheck(grid, i, j, "T")) {
+            placeTile(i, j, (floor(random(4))), 0);
+            placeTile(i, j, 14, random([0, 6, 15]));
+        }
+        
+        /*if(grid[i][j] == '.'){
+          //room floor
+          placeTile(i, j, (floor(random(11, 14))), (floor(random(22, 25))));
+          //room wall
+          if(grid[i-1][j] == '_'){
+            //top
+            placeTile(i, j, 5, 0);
+            //corners (left then right)
+            if(grid[i][j-1] == '_'){
+              placeTile(i, j, 4, 0);
+            }
+            if(grid[i][j+1] == '_'){
+              placeTile(i, j, 6, 0);
+            }
+          }
+          
+          if(grid[i+1][j] == '_'){
+            //bottom
+            placeTile(i, j, 5, 2);
+            //corners (left then right)
+            if(grid[i][j-1] == '_'){
+              placeTile(i, j, 4, 2);
+            }
+            if(grid[i][j+1] == '_'){
+              placeTile(i, j, 6, 2);
+            }
+          }
+          
+          if(grid[i][j-1] == '_'){
+            //left
+            placeTile(i, j, 4, 1);
+          }
+          if(grid[i][j+1] == '_'){
+            //right
+            placeTile(i, j, 6, 1);
+          }
+        }*/
+      }
+    }
+  }
+  
+  //If location i,j is inside the grid (not out of bounds), does grid[i][j]==target? Otherise, return false.
+  function gridCheck(grid, i, j, target) {
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+      return false;
+    }
+    return grid[i][j] === target;
+  }
+  
+  //Form a 4-bit code using gridCheck on the north/south/east/west neighbors of i,j for the target code. You might use an example like (northBit<<0)+(southBit<<1)+(eastBit<<2)+(westBit<<3).
+  function gridCode(grid, i, j, target) {
+    let northBit = gridCheck(grid, i-1, j, target) ? 1 : 0;
+    let southBit = gridCheck(grid, i+1, j, target) ? 1 : 0;
+    let eastBit = gridCheck(grid, i, j+1, target) ? 1 : 0;
+    let westBit = gridCheck(grid, i, j-1, target) ? 1 : 0;
+
+    return (northBit << 0) + (southBit << 1) + (eastBit << 2) + (westBit << 3);
+  }
+  
+  //Get the code for this location and target. Use the code as an array index to get a pair of tile offset numbers.
+  function drawContext(grid, i, j, target, dti, dtj) {
+    const code = gridCode(grid, i, j, target);
+    const [tiOffset, tjOffset] = lookupO[code];
+
+    //calculate tile position offset for correct border placement
+    let northBit = gridCheck(grid, i-1, j, target) ? 1 : 0;
+    let southBit = gridCheck(grid, i+1, j, target) ? 1 : 0;
+    let eastBit = gridCheck(grid, i, j+1, target) ? 1 : 0;
+    let westBit = gridCheck(grid, i, j-1, target) ? 1 : 0;
+    
+    //border offset
+    let xOffset = (-northBit) + (southBit);
+    let yOffset = (-eastBit) + (westBit);
+
+    placeTile(i, j, dti + tiOffset, dtj + tjOffset);
+  }
+  
+  //A global variable referring to an array of 16 elements. Fill this with hand-typed tile offset pairs, e.g. [2,1], so that drawContext does not need to handle any special cases.
+  const lookupO = [
+    [1, 1],
+    [1, 0],
+    [1, 2],
+    [1, 1],
+    [2, 1],
+    [2, 0],
+    [2, 2],
+    [2, 1],
+    [0, 1],
+    [0, 0],
+    [0, 2],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, 2],
+    [1, 1]
+  ];
+
+});
